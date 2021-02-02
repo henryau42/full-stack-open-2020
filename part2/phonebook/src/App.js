c48f43b144414db1ce9b293497ac65a09ee5dc56
Henry Au <henry.au@moesol.com>
2021-02-02 12:33:00 -0800

    Exercise 2.9 - the Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 import Numbers from "./components/Numbers";
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '000-000-0000' }
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
+    const [ filteredList, setFilteredList ] = useState(persons)
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
+    const [ newSearch, setNewSearch ] = useState('')
 
     const addName = (event) => {
         event.preventDefault()
@@ -21,6 +26,8 @@ const App = () => {
             setPersons(persons.concat(personObject))
             setNewName('')
             setNewNumber('')
+
+            if(newSearch === '') setFilteredList(filteredList.concat(personObject))
         }
 
     }
@@ -33,6 +40,13 @@ const App = () => {
         setNewNumber(event.target.value);
     }
 
+    const handleSearchChange = (event) => {
+        const search = event.target.value
+        const filter = persons.filter(person => person.name.toLowerCase().includes(search))
+        setFilteredList(filter)
+        setNewSearch(event.target.value);
+    }
+
     const checkDuplicate = (newName) => {
         const duplicate = persons.filter(person => person.name.toLowerCase() === newName.toLowerCase())
         return duplicate.length > 0
@@ -41,6 +55,14 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with
+                <input
+                    value={newSearch}
+                    onChange={handleSearchChange}
+                />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={addName}>
                 <div>
                     name:
@@ -61,7 +83,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            <Numbers persons={persons} />
+            <Numbers persons={filteredList} />
         </div>
     )
 }
--- part2/phonebook/src/components/Numbers.js
+++ part2/phonebook/src/components/Numbers.js
@@ -21,7 +21,6 @@ const Names = ({ persons }) => {
     )
 }
 
-
 const Numbers = ({ persons }) => {
     return (
         <div>
