03e6123b760e6836cbe10ef51e339dcc7110163c
Henry Au <henry.au@moesol.com>
2021-02-04 17:00:56 -0800

    Exercise 2.11 - the Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,39 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
-    const [ filteredPersons, setFilteredPersons ] = useState(persons)
-    const [ newSearch, setNewSearch ] = useState('')
+  const [persons, setPersons] = useState([])
+  const [ filteredPersons, setFilteredPersons ] = useState([])
+  const [ newSearch, setNewSearch ] = useState('')
 
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <Filter
-                persons={persons}
-                newSearch={newSearch}
-                setNewSearch={setNewSearch}
-                setFilteredPersons={setFilteredPersons}
-            />
-            <h2>Add a new</h2>
-            <PersonForm
-                persons={persons}
-                setPersons={setPersons}
-                newSearch={newSearch}
-                filteredPersons={filteredPersons}
-                setFilteredPersons={setFilteredPersons}
-            />
-            <h2>Numbers</h2>
-            <Persons persons={filteredPersons} />
-        </div>
-    )
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter
+          persons={persons}
+          newSearch={newSearch}
+          setNewSearch={setNewSearch}
+          setFilteredPersons={setFilteredPersons}
+      />
+      <h2>Add a new</h2>
+      <PersonForm
+          persons={persons}
+          setPersons={setPersons}
+          newSearch={newSearch}
+          filteredPersons={filteredPersons}
+          setFilteredPersons={setFilteredPersons}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={filteredPersons} />
+    </div>
+  )
 }
 
 export default App
