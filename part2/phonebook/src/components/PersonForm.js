6a2e4721664dfca6f03d7968fa75ebb0e2f2155a
Henry Au <henry.au@moesol.com>
2021-02-08 17:21:55 -0800

    Exercise 2.15: Phonebook step7

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,62 +1,70 @@
 import React, { useState } from 'react';
+import axios from "axios";
 
 const PersonForm = ({ persons, setPersons, newSearch, filteredPersons, setFilteredPersons }) => {
-    const [ newName, setNewName ] = useState('')
-    const [ newNumber, setNewNumber ] = useState('')
+		const [ newName, setNewName ] = useState('')
+		const [ newNumber, setNewNumber ] = useState('')
 
-    const handleNameChange = (event) => {
-        setNewName(event.target.value);
-    }
+		const handleNameChange = (event) => {
+				setNewName(event.target.value);
+		}
 
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value);
-    }
+		const handleNumberChange = (event) => {
+				setNewNumber(event.target.value);
+		}
 
-    const checkDuplicate = (newName) => {
-        const duplicate = persons.filter(person => person.name.toLowerCase() === newName.toLowerCase())
-        return duplicate.length > 0
-    }
+		const checkDuplicate = (newName) => {
+				const duplicate = persons.filter(person => person.name.toLowerCase() === newName.toLowerCase())
+				return duplicate.length > 0
+		}
 
-    const addName = (event) => {
-        event.preventDefault()
+		const addName = (event) => {
+				event.preventDefault()
 
-        if(checkDuplicate(newName)) {
-            alert(`${newName} is already added to phonebook`)
-        } else {
-            const personObject = {
-                name: newName,
-                number: newNumber
-            }
-            setPersons(persons.concat(personObject))
-            setNewName('')
-            setNewNumber('')
+				if(checkDuplicate(newName)) {
+						alert(`${newName} is already added to phonebook`)
+				} else {
+						const personObject = {
+								name: newName,
+								number: newNumber
+						}
 
-            if(newSearch === '') setFilteredPersons(filteredPersons.concat(personObject))
-        }
+						axios
+							  .post('http://localhost:3001/persons', personObject)
+							  .then(response => {
+									  console.log(response)
+							  })
 
-    }
+						setPersons(persons.concat(personObject))
+						setNewName('')
+						setNewNumber('')
 
-    return (
-        <form onSubmit={addName}>
-            <div>
-                name:
-                <input
-                    value={newName}
-                    onChange={handleNameChange}
-                />
-            </div>
-            <div>
-                number:
-                <input
-                    value={newNumber}
-                    onChange={handleNumberChange}
-                />
-            </div>
-            <div>
-                <button type="submit">add</button>
-            </div>
-        </form>
-    )
+						if(newSearch === '') setFilteredPersons(filteredPersons.concat(personObject))
+				}
+
+		}
+
+		return (
+				<form onSubmit={addName}>
+						<div>
+								name:
+								<input
+										value={newName}
+										onChange={handleNameChange}
+								/>
+						</div>
+						<div>
+								number:
+								<input
+										value={newNumber}
+										onChange={handleNumberChange}
+								/>
+						</div>
+						<div>
+								<button type="submit">add</button>
+						</div>
+				</form>
+		)
 }
 
 export default PersonForm
